2c52536ece50219c22432d739cd28e427b0a73ab
Grihit <grihitbudhiraja12@gmail.com>
2021-05-17 14:21:46 +0530

    changes made

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              9827374890 | For any queries contact Quality Assurance Team Lead
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
