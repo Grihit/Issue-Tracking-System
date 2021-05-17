97d1ce310d938d5ffb5c7cff53c12ae55cc37109
shakti-7 <shakti.balaji712@gmail.com>
2021-05-17 19:56:39 +0530

    Title changes

--- admin/index.php
+++ admin/index.php
@@ -35,7 +35,7 @@ exit();
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>VITCC CMS | Admin login</title>
+	<title>Issue Tracking System | Admin login</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
@@ -52,7 +52,7 @@ exit();
 				</a>
 
 			  	<a class="brand" href="index.html">
-			  		VITCC CMS | Admin
+			  		Issue Tracking System | Admin
 			  	</a>
 
 				<div class="nav-collapse collapse navbar-inverse-collapse">
